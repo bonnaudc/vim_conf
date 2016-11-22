ef31a4d4851dd6c359dff7866a076e18e190213a
cyrillebonnaud <bonnaudc@gmail.com>
2016-11-22 22:00:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/cyrillebonnaud/vim_conf.git
 cd vim_conf
 git submodule init && git submodule update
-ln -rs vim_conf/.vimrc ~/
-ln -rs vim_conf/.vim ~/
+ln -rs ./.vimrc ~/
+ln -rs ./.vim ~/
